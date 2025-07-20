2a8b2d8c4a9dc4863be3b3b54b1d92178c323cf2
nikilr7 <nikilraja11@gmail.com>
2025-07-21 00:11:07 +0530

    SRC

---
+++ Libraries/src/test/java/org/nik/Libraries/LibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nik.Libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
